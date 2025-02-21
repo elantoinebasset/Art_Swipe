7c9a5e5e0142cc589ad0ced143beedc732f073b0
Leon Carter <leoncarter4200@gmail.com>
2025-02-21 14:55:46 +0100

    Update - MAIN PAGE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#main"
   resources :artworks
   resources :artists
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,5 +13,4 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root 'artists#index'
 end
